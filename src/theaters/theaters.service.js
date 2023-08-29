48c7cb6982a6ba4c9cb0547c037387bd2b4f9d2d
MTaslam2000 <taiyab.aslam728@gmail.com>
2023-08-29 14:51:14 -0500

    got my final test to pass involving the list theaters

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,7 +1,21 @@
 const knex = require("../db/connection.js");
+const reduceProperties = require("../utils/reduce-properties.js");
+
+const reduceTheaterAndMovies = reduceProperties("theater_id", {
+  movie_id: ["movies", null, "movie_id"],
+  title: ["movies", null, "title"],
+  rating: ["movies", null, "rating"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+  description: ["movies", null, "description"],
+  image_url: ["movies", null, "image_url"],
+});
 
 async function list() {
-  return knex("theaters").select("*");
+  return knex("theaters as t")
+    .select("*")
+    .join("movies_theaters as mt", "mt.theater_id", "t.theater_id")
+    .join("movies as m", "m.movie_id", "mt.movie_id")
+    .then(reduceTheaterAndMovies);
 }
 
 module.exports = {
