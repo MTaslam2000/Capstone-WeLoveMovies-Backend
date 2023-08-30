88fa17bc299bf154c9950836e3ba8951bfe468ce
MTaslam2000 <taiyab.aslam728@gmail.com>
2023-08-30 13:53:28 -0500

    updated my reduceTheaterAndMovies function

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -8,6 +8,10 @@ const reduceTheaterAndMovies = reduceProperties("theater_id", {
   runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
   description: ["movies", null, "description"],
   image_url: ["movies", null, "image_url"],
+  is_showing: ["movies", null, "is_showing"],
+  theater_id: ["movies", null, "theater_id"],
+  created_at: ["movies", null, "created_at"],
+  updated_at: ["movies", null, "updated_at"],
 });
 
 async function list() {
